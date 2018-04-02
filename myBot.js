2592b1ed412501cbb3ee827e9d720377ca59cb0a
Cody Wolter <cwolter97@gmail.com>
2018-04-02 03:16:04 -0500

    bug-fixing

--- myBot.js
+++ myBot.js
@@ -67,9 +67,7 @@ bot.on("message", async message => {
           //make request for stuff
           request(options, function (error, response, body) {
             let jsonResponse = JSON.parse(body);
-
-            console.log(body);
-
+            //console.log(body);
             video_id = jsonResponse.items[0].id.videoId;
             video_title =jsonResponse.items[0].snippet.title;
 
@@ -84,7 +82,9 @@ bot.on("message", async message => {
 
           voiceChannel.join()
             .then(connection => {
-              const stream = ytdl('https://www.youtube.com/watch?v={video_id}', { filter : 'audioonly' });
+              console.log("INCONNECTION title: " + video_title);
+              console.log("INCONNECTION id: " + video_id);
+              const stream = ytdl('https://www.youtube.com/watch?v=' + video_id, { filter : 'audioonly' });
               const dispatcher = connection.playStream(stream, streamOptions);
             })
             .catch(console.error);
